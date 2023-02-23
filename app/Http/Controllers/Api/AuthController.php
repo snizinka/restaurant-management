bc9897ab23e22d4d3a02ec1f8b1df706fe3fa6fe
Bohdan Oskin <bogdanosckinss@gmail.com>
2023-02-23 17:32:37 +0200

    Login api functionality created

---
+++ app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\LoginUserRequest;
+use App\Http\Requests\StoreUserRequest;
+use App\Models\User;
+use App\Traits\HttpResponses;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    use HttpResponses;
+
+    public function login(LoginUserRequest $request) {
+        $request->validated($request->all());
+
+        if (!Auth::attempt($request->only('email', 'password'))) {
+            return $this->error('', 'Wrong auth data', 401);
+        }
+
+        $user = User::where('email', $request->email)->first();
+        return $this->success([
+            'user' => $user,
+            'token' => User::createToken('Api token'.$user->name)->plainTextToken
+        ]);
+    }
+
+    public function register(StoreUserRequest $request) {
+        $request->validated($request->all());
+        $user = User::create([
+           'name' => $request->name,
+           'email' => $request->email,
+           'password' => Hash::make($request->password)
+        ]);
+
+        return $this->success([
+            'user' => $user,
+            'token' => $user->createToken('Api token'.$user->name)->plainTextToken
+        ]);
+    }
+
+    public function logout() {
+        return response()->json('Registration');
+    }
+}
