5e77f0734e91028871ce2d5a93a564569928472a
Bohdan Oskin <bogdanosckinss@gmail.com>
2023-02-23 19:50:48 +0200

    Created migrations (restaurants, dish_categories, dishes) for dishes api

---
+++ database/migrations/2023_02_23_174257_create_dishes_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('dishes', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('restaurant_id');
+            $table->unsignedBigInteger('category_id');
+            $table->string('image')->nullable();
+            $table->string('name');
+            $table->decimal('price');
+            $table->string('ingredients');
+            $table->foreign('restaurant_id')->references('id')->on('restaurants')->onDelete('cascade');
+            $table->foreign('category_id')->references('id')->on('dish_categories')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('dishes');
+    }
+};
