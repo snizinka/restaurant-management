68d6bf907880e5029c77c40b36601fbe5a3f8456
Bohdan Oskin <bogdanosckinss@gmail.com>
2023-02-23 21:27:29 +0200

    Replaced dish controller

--- app/Http/Controllers/Api/DishesController.php
+++ app/Http/Controllers/Api/DishesController.php
@@ -1,7 +1,8 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Api;
 
+use App\Http\Controllers\Controller;
 use App\Http\Requests\StoreDishRequest;
 use App\Http\Resources\DishesResource;
 use App\Models\Dish;
--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,10 @@
 <?php
 
+use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\DishesController;
+use App\Http\Controllers\Api\RestaurantController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Api\AuthController;
-use App\Http\Controllers\DishesController;
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
@@ -15,5 +16,6 @@ Route::post('/register', [AuthController::class, 'register']);
 
 Route::group(['middleware' => ['auth:sanctum']], function () {
     Route::post('/logout', [AuthController::class, 'logout']);
+    Route::get('/restaurant/{id}', [RestaurantController::class, 'dishesList']);
     Route::resource('/dishes', DishesController::class);
 });
